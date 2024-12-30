3a7b09265d94da058c5074d155c67600d180c0b9
MrMalloc <148658081+mdrapercodam@users.noreply.github.com>
2024-12-30 11:49:22 +0100

    Create ft_free_values.c

---
+++ src/libft/ft_free/ft_free_values.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   1_free.c                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: mdraper <mdraper@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2024/12/29 11:57:47 by mdraper       #+#    #+#                 */
+/*   Updated: 2024/12/30 11:46:20 by mdraper       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void ft_free_double(double **value)
+{
+	if (!value || !*value)
+		return ;
+	free(*value);
+	*value = NULL;
+}
